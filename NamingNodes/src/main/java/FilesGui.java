f21519af55246b0f87c809ec595a4bad2564e39c
Cedric Plouvier <cedricplouvier@Cedrics-MacBook-Pro-2.local>
2019-09-04 18:38:08 +0200

    listfiles poging 10000

--- NamingNodes/src/main/java/FilesGui.java
+++ NamingNodes/src/main/java/FilesGui.java
@@ -18,19 +18,17 @@ public class FilesGui extends JFrame {
         setSize(300, 200);
         add(rootpanel);
         System.out.println("rootpanel added");
-        String[] columns = new String[] {"file"};
-        System.out.println("collums created");
-        DefaultTableModel defaultModel = new DefaultTableModel(columns, 0);
+        DefaultTableModel defaultModel = new DefaultTableModel();
         System.out.println("default table created");
 
         for (Map.Entry<String, FileProperties> entry : node.filenameMap.entrySet()) {
             System.out.println("table entries added");
-            defaultModel.addRow(new Object[] {entry.getKey()});
+            defaultModel.addColumn(entry.getKey());
         }
+        table1.setModel(defaultModel);
         //JList list1 = new JList((ListModel) node.filenameMap);
         //listFiles();
         //JList list1 = new JList((ListModel) node.filenameMap);
-        rootpanel.add(table1);
         setVisible(true);
 
 
